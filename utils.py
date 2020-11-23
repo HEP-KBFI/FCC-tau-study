d2b1fa863f874bb83a60e69a10ce319ee0b4db20
Diana Rand <diana.rand@outlook.com>
2020-11-23 15:59:58 +0200

    Added potentially useful functions to utils

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 # Useful functions
 
-from ROOT import TLorentzVector
+from ROOT import Math, TLorentzVector
 
 
 def get_lorentz_vector(particle):
-    # Get the Lorentz Vector of a given particle
+    # Get the Lorentz Vector of a given particle (Legacy TLorentzVector class)
     vector = TLorentzVector()
     vector.SetXYZM(
         particle.core.p4.px,
@@ -12,6 +12,16 @@ def get_lorentz_vector(particle):
         particle.core.p4.pz,
         particle.core.p4.mass
     )
+    return vector
+
+
+def get_lorentz_vector_new(particle):
+    # Get the Lorentz Vector of a given particle (Math::LorentzVector class)
+    vector = Math.PxPyPzMVector()
+    vector.SetPx(particle.core.p4.px)
+    vector.SetPy(particle.core.p4.py)
+    vector.SetPz(particle.core.p4.pz)
+    vector.SetM(particle.core.p4.mass)
     return vector
 
 
@@ -29,12 +39,20 @@ def calculate_mass(particles):
 
 def check_pt(vector, limit):
     # Check whether the transverse momentum corresponding to a given Lorentz vector is over the given limit (GeV)
-    pT = vector.Perp()
+    pT = get_pt(vector)
     if pT > limit:
         return True
     return False
 
 
+def get_pt(vector):
+    # Get pT from a given Lorentz vector
+    pT = None
+    try:
+        pT = vector.Perp()
+    except AttributeError:
+        pT = sqrt(vector.Perp2())
+    return pT
 
 # class Particle:
 #     def __init__(self, data):
