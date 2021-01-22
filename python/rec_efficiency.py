7eebd2f488510e4b7b05d4589b8ae6095eb60c2e
Diana Rand <diana.rand@outlook.com>
2021-01-22 15:21:28 +0200

    Code refactoring work in progress

--- python/rec_efficiency.py
+++ python/rec_efficiency.py
@@ -4,15 +4,18 @@ from ROOT import TFile, TEfficiency
 import utils
 
 
-class TauSet:
+class EventTauFinder:
     def __init__(self, tree):
         # Initialize
         self.tree = tree
         self.count = 0
+        # Count generator level taus
         for particle in tree.skimmedGenParticles:
             if abs(particle.core.pdgId) == 15 and particle.core.status == 2:
                 self.count += 1
-        self.taus = []
+        self.taus = []  # List of true taus
+        self.fakes = []  # List of fake taus
+        # Create data objects for storing information about tau jets
         for i in range(self.count):
             self.taus.append(TauJet())
         # Find taus
@@ -20,12 +23,15 @@ class TauSet:
         self.__get_rec_taus()
 
     def __len__(self):
+        # Return number of taus
         return self.count
 
     def __getitem__(self, item):
+        # Return one tau with given index
         return self.taus[item]
 
     def __get_gen_taus(self):
+        # Sort through generator level tau and calculate tau jets
         particles = tree.skimmedGenParticles
         neutrinos = []
         previous_states = []
@@ -50,6 +56,7 @@ class TauSet:
             self.__add_states(state)
 
     def __get_rec_taus(self):
+        # Sort through reconstructed tau jets
         jets = self.tree.jets
         tags = self.tree.tauTags
         for i in range(len(jets)):
@@ -58,36 +65,44 @@ class TauSet:
             self.__add_reconstruction(jets[i])
 
     def __add_tau(self, tau):
+        # Save generator level tau particle
         for obj in self.taus:
             if not obj.gen_tau:
                 obj.gen_tau = tau
                 break
 
     def __add_neutrino(self, neutrino):
+        # Add corresponding neutrinos to generator level tau particles
         for obj in self.taus:
-            # print("Checking tau with ID ", obj.gen_tau.core.pdgId)
             if not obj.gen_neutrino:
-                # print("Neutrino not found, attempting to match...")
                 if obj.match_neutrino(neutrino):
                     break
 
     def __add_states(self, state):
+        # Save information about generator level tau particle's previous states
         for obj in self.taus:
             if obj.match_previous_states(state):
                 break
 
     def __add_reconstruction(self, jet):
+        # Match a reconstruction to a generator level tau
         for obj in self.taus:
             if not obj.rec_taujet:
                 if obj.match_reconstruction(jet):
-                    break
+                    return
+        self.fakes.append(jet)
 
-    def num_rec_taus(self):
+    def count_recs(self):
+        # Return number of correctly reconstructed tau jets
         count = 0
         for tau in self.taus:
             if tau.check_reconstructed():
                 count += 1
         return count
+
+    def count_fakes(self):
+        # Return number of fake taus
+        return len(self.fakes)
 
 
 class TauJet:
@@ -102,6 +117,7 @@ class TauJet:
         self.rec_vector = None
 
     def match_neutrino(self, neutrino):
+        # Match a tau neutrino to a generator level tau particle
         if self.gen_tau.core.pdgId * neutrino.core.pdgId > 0:
             self.gen_neutrino = neutrino
             self.gen_vector = utils.get_lorentz_vector(self.gen_tau) - utils.get_lorentz_vector(neutrino)
@@ -109,6 +125,7 @@ class TauJet:
         return False
 
     def match_previous_states(self, state):
+        # Match generator level information about previous states to a tau particle
         if self.gen_tau.core.charge != state.core.charge:
             return False
         deltaR = self.gen_vector.DeltaR(utils.get_lorentz_vector(state))
@@ -118,6 +135,7 @@ class TauJet:
         return False
 
     def match_reconstruction(self, jet):
+        # Match a reconstructed tau jet to a generator level tau
         if self.rec_taujet:
             return False
         vector = utils.get_lorentz_vector(jet)
@@ -129,185 +147,22 @@ class TauJet:
         return False
 
     def check_reconstructed(self):
+        # Check whether a tau has a reconstruction
         if self.rec_taujet:
             return True
         return False
 
-
-# class Tau:
-#     # Data object to save data about a tau particle
-#     def __init__(self, tau):
-#         self.tau = tau
-#
-#     def pt(self):
-#         # Shorthand function for getting the transverse momentum
-#         return utils.get_pt(self.vector)
-
-
-# class GeneratorTauJet(Tau):
-#     # Data object to save information about a generator level tau jet and associate it with its other states
-#     def __init__(self, tau, neutrino):
-#         # Tau jet
-#         super().__init__(tau)
-#         self.neutrino = neutrino
-#         self.vector = utils.get_lorentz_vector(self.tau) - utils.get_lorentz_vector(self.neutrino)
-#         # Associations
-#         self.final_state = None
-#         self.previous_states = []
-#         self.reconstruction = None
-#
-#     def add_state(self, candidate):
-#         # Check if candidate tau is a previous state of the given generator tau and add it
-#         candidate = GeneratorTau(candidate)
-#         if self.get_charge() != candidate.get_charge():
-#             return False
-#         deltaR = self.vector.DeltaR(candidate.vector)
-#         if deltaR < 0.05:
-#             candidate.final_state = self
-#             self.previous_states.append(candidate)
-#             return True
-#         return False
-#
-#     def get_charge(self):
-#         # Return the charge of the tau
-#         return self.tau.core.charge
-#
-#     def check_reconstruction(self):
-#         # Check if the tau has a matching reconstruction
-#         if self.reconstruction:
-#             return True
-#         return False
-#
-#
-# class ReconstructedTau(Tau):
-#     # Data object to associate reconstructed tau jet to corresponding generator tau
-#     def __init__(self, data):
-#         # Tau jet
-#         super().__init__(data)
-#         self.vector = utils.get_lorentz_vector(self.tau)
-#         # Associations
-#         self.generator_match = None
-#
-#     def add_generator_match(self, candidate):
-#         # Check if corresponding generator tau already exists
-#         if self.generator_match:
-#             return False
-#         # Check if candidate generator tau corresponds
-#         deltaR = self.vector.DeltaR(candidate.vector)
-#         if deltaR < 0.05:
-#             self.generator_match = candidate
-#             if candidate.final_state:
-#                 candidate.final_state.reconstruction = self
-#             else:
-#                 candidate.reconstruction = self
-#             return True
-#         return False
-#
-#     def check_fake(self):
-#         # Return true if no matching generator tau jet
-#         if not self.generator_match:
-#             return True
-#         return False
-
-
-# def get_gen_taus(tree):
-#     # Get generator level taus as final taus with corresponding previous states
-#     gen_particles = tree.skimmedGenParticles
-#     tau_jets = []
-#     final_taus = []
-#     previous_states = []
-#     neutrinos = []
-#
-#     # Find generator level taus and tau neutrinos
-#     for particle in gen_particles:
-#         if abs(particle.core.pdgId) == 16:
-#             neutrinos.append(particle)
-#             continue
-#
-#         elif abs(particle.core.pdgId) != 15:
-#             continue
-#
-#         if particle.core.status == 2:
-#             final_taus.append(particle)
-#         else:
-#             previous_states.append(particle)
-#
-#     # Match neutrinos to taus and create objects
-#     for tau in final_taus:
-#         matched = False
-#         for neutrino in neutrinos:
-#             if matched:
-#                 break
-#             if tau.core.charge == neutrino.core.charge:
-#                 tau_jets.append(GeneratorTauJet(tau, neutrino))
-#                 matched = True
-#
-#     # Match previous states to final taus
-#     for tau in previous_states:
-#         for jet in tau_jets:
-#             if jet.add_state(tau):
-#                 continue
-#
-#     return tau_jets
-
-#
-# def get_rec_taus(tree):
-#     # Get reconstructed tau jets
-#     jets = tree.jets
-#     tau_tags = tree.tauTags
-#     rec_taus = []
-#     for i in range(len(jets)):
-#         if tau_tags[i].tag < 0.5:
-#             continue
-#         rec_taus.append(ReconstructedTau(jets[i]))
-#     return rec_taus
-
-
-# def match_taus(gen_taus, rec_taus):
-#     for tau in gen_taus:
-#         match = False
-#         for rec_tau in rec_taus:
-#             match = rec_tau.add_generator_match(tau)
-#             break
-#         if not match:
-#             for previous_state in tau.previous_states:
-#                 if match:
-#                     break
-#                 for rec_tau in rec_taus:
-#                     if rec_tau.add_generator_match(previous_state):
-#                         break
-#
-#         # Try to match a generator tau to a tau tagged reconstructed jet
-#         for rec_tau in rec_taus:
-#             if rec_tau.add_generator_match(tau):
-#                 return True
-#     # # Try to match any previous state of a generated tau to a reconstructed jet
-#     # for previous_state in gen_tau.previous_states:
-#     #     for rec_tau in rec_taus:
-#     #         if rec_tau.add_generator_tau(previous_state):
-#     #             return True
-#     return False
+    def pt(self):
+        # Return the pT of the tau
+        return utils.get_pt(self.gen_vector)
 
 
 def fill_histograms(taus, efficiency_histo):
-    # gen_taus = get_gen_taus(tree)
-    # rec_taus = get_rec_taus(tree)
-    #
-    # for tau in gen_taus:
-    #     # Try to match a final state  of a generated tau to a reconstructed jet
-    #     match = match_taus(tau, rec_taus)
-    #
-    #     if not match:
-    #     # Try to match any previous state of a generated tau to a reconstructed jet
-    #         for previous_state in tau.previous_states:
-    #             match = match_taus(previous_state, rec_taus)
-    #             if match:
-    #                 break
-
-    # Fill efficiency histogram
+    # Fill histograms
     for i in range(len(taus)):
+        # Fill efficiency histogram
         efficiency_histo.Fill(taus[i].check_reconstructed(), taus[i].pt())
-    #
+
     # # Fill fake rate histogram
     # for tau in rec_taus:
     #     fake_histo.Fill(tau.check_fake(), tau.pt())
@@ -318,36 +173,28 @@ inf = TFile('data/delphes_output.root')
 outf = TFile('data/rec_efficiency.root', 'RECREATE')
 
 # Create histograms
-# efficiency_histo = TEfficiency('efficiency', 'efficiency', 8, 0, 120)
+efficiency_histo = TEfficiency('efficiency', 'efficiency', 10, 0, 90)
 # fake_histo = TEfficiency('fake rate', 'fake rate', 8, 0, 120)
 
-n_tag = 0
-n_true = 0
+n_tag = 0  # Count of correctly tagged true taus
+n_true = 0  # Count of true taus
 
 # Read events
 tree = inf.Get('events')
 n_tot = tree.GetEntries()
 for event in range(n_tot):
     tree.GetEntry(event)
-    # gen_taus = get_gen_taus(tree)
-    # rec_taus = get_rec_taus(tree)
-    #
-    # match_taus(gen_taus, rec_taus)
-    #
-    # for tau in gen_taus:
-    #     n_true += 1
-    #     if tau.matching_reconstruction:
-    #         n_tag += 1
+    taus = EventTauFinder(tree)
 
-    taus = TauSet(tree)
-
+    # Count taus
     n_true += len(taus)
-    n_tag += taus.num_rec_taus()
+    n_tag += taus.count_recs()
 
-    # fill_histograms(taus, efficiency_histo)
+    fill_histograms(taus, efficiency_histo)
 
 # Write histograms to file
 print("Generated taus: ", n_true)
 print("Correctly tagged taus: ", n_tag)
-# print("Efficiency over entire dataset: ", n_tag / n_true)
-# outf.Write()
+print("Efficiency over entire dataset: ", n_tag / n_true)
+
+outf.Write()
